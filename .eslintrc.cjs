8ae04f84118737e6713b811dc54949013735b394
XH W <xhw@XHs-MacBook-Pro.local>
2023-07-05 12:20:13 +0800

    初始化项目

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,5 +11,24 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        semi: false,
+        printWidth: 80,
+        trailingComma: 'none',
+        endOfLine: 'auto'
+      }
+    ],
+    'vue/multi-word-component-names': [
+      'warn',
+      {
+        ignores: ['index']
+      }
+    ],
+    'vue/no-setup-props-destructure': ['off']
   }
 }
